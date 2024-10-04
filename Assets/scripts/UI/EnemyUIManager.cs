0dc85fcd2d0bae9f5610d48b01c30ca2a1c53220
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-10-04 10:42:54 +0200

    <feat>[Game] debug

---
+++ Assets/scripts/UI/EnemyUIManager.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class EnemyUIManager {
+
+   // UI image to display the Enemy
+    public Image EnemySprite; 
+    public Sprite Enemy1;
+    public Sprite Enemy2;
+    public Sprite Enemy3;
+    public Sprite Enemy4;
+
+    public EnemyUI enemyUI; 
+    public Character character;
+
+
+    public void DisplayEnemies()
+{
+    Debug.Log(">DISPLAY ENEMY");
+    
+
+    if (GameController.Instance != null && GameController.Instance.player != null)
+    {
+        foreach (Character enemies in GameController.Instance.enemy.enemies)
+        {
+            Debug.Log($"Enemy Name: {enemies.characterName}");
+            Debug.Log($"Enemy Health: {enemies.health}");
+            Debug.Log($"Enemy Level: {enemies.level}");
+            enemyUI.Initialize(character);
+
+        }
+    }
+    else
+    {
+        Debug.LogError("GameController or Player is null!");
+    }
+}
+
+
+}

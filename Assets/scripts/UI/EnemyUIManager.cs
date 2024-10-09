a439de8af6dd0b5188f41be2ed609e6f3ab1b3eb
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-10-09 14:03:36 +0200

    <feat>[world] merged UI from game to world

--- Assets/scripts/UI/EnemyUIManager.cs
+++ Assets/scripts/UI/EnemyUIManager.cs
@@ -3,37 +3,39 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class EnemyUIManager : MonoBehaviour{
+public class EnemyUIManager : MonoBehaviour
+{
 
-   // UI image to display the Enemy
+    // UI image to display the Enemy
     // public Image EnemySprite; 
     // public Sprite Enemy1;
     // public Sprite Enemy2;
     // public Sprite Enemy3;
     // public Sprite Enemy4;
 
-    public EnemyUI enemyUI; 
+    public EnemyUI enemyUI;
     // public Character character;
 
 
     public void DisplayEnemies()
-{
-    Debug.Log(">DISPLAY ENEMY");
-    
+    {
+        Debug.Log(">DISPLAY ENEMY");
+    }
 
-//     if (GameController.Instance != null && GameController.Instance.player != null)
-//     {
-//         foreach (Character enemies in GameController.Instance.enemy.enemies)
-//         {
-//             Debug.Log($"Enemy Name: {enemies.characterName}");
-//             Debug.Log($"Enemy Health: {enemies.health}");
-//             Debug.Log($"Enemy Level: {enemies.level}");
-//             enemyUI.Initialize(character);
 
-//         }
-//     }
-//     else
-//     {
-//         Debug.LogError("GameController or Player is null!");
-//     }
+    //     if (GameController.Instance != null && GameController.Instance.player != null)
+    //     {
+    //         foreach (Character enemies in GameController.Instance.enemy.enemies)
+    //         {
+    //             Debug.Log($"Enemy Name: {enemies.characterName}");
+    //             Debug.Log($"Enemy Health: {enemies.health}");
+    //             Debug.Log($"Enemy Level: {enemies.level}");
+    //             enemyUI.Initialize(character);
+
+    //         }
+    //     }
+    //     else
+    //     {
+    //         Debug.LogError("GameController or Player is null!");
+    //     }
 }

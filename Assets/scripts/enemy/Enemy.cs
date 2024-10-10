26b6866354b55be271d7f45a44ad2d7d7e8eb145
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-10-10 14:29:40 +0200

    Refactor Enemy class to include enemy type and rarity

--- Assets/scripts/enemy/Enemy.cs
+++ Assets/scripts/enemy/Enemy.cs
@@ -1,14 +1,34 @@
-// using UnityEngine; 
-// using System.Collections.Generic;
+using UnityEngine; 
+using System.Collections.Generic;
+using JetBrains.Annotations;
+[System.Serializable]
+public class Enemy : Character
+{
 
-// public class Enemy
-//{
+    public List <Character> enemies{get; set;}
 
-//     public List <Character> enemies{get; set;}
+    public Enemy( string characterName, int health, int maxHealth, int strength, int defense, int speed, int level, int experience, int experienceToNextLevel, int characterID) 
+    : base(characterName, health, maxHealth, strength, defense, speed, level, experience, experienceToNextLevel, characterID){
+        enemies = new List<Character>();
+    }
 
-//     public Enemy() {
-//         enemies = new List<Character>(); 
-//     }
+    public enum Type{
+        ORC,
+        PROWLERORC,
+        WARRIORORC,
+        TROLL
+    }
 
+    public enum Rarity{
+        COMMON,
+        UNCOMMON,
+        RARE,
+        SUPERRARE
+    }
 
-// }
+    public Type EnemyType;
+    public Rarity EnemyRarity;
+
+    public int baseAttack, baseDefense;
+    public int curAttack, curDefense;
+}
